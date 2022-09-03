93437428345dbba8b43e5623237ca4b952d31bde
Peekoe <peekoecode@gmail.com>
2022-09-02 21:52:48 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import devLogo from './assets/dev-logo.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://acmcsuf.com" target="_blank"> 
+      <img src={devLogo} class="logo dev" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Svelte Portfolio Template</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.dev:hover {
+    filter: drop-shadow(0 0 2em #1E6CFF);
+  }
+</style>
