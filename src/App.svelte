7d692cd0f756e455c77785937d80864f187cfa1c
anhduy1202 <aduy1122@gmail.com>
2022-10-07 17:07:15 -0700

    we've made the navbar git add .

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,12 @@
 <script>
-  import devLogo from './assets/dev-logo.svg'
-  import Counter from './lib/Counter.svelte'
+  import NavBar from "./components/NavBar.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://acmcsuf.com" target="_blank"> 
-      <img src={devLogo} class="logo dev" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Svelte Website Template</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <NavBar/>
+  
 </main>
 
 <style>
-  .logo {
-    height: em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
 
-  .logo.dev:hover {
-    filter: drop-shadow(0 0 2em #1E6CFF);
-  }
 </style>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,41 @@
+<script>
+    const navItems = [
+        {title: "About", url: "#About"},
+        {title: "Experiences", url: "#Experiences"},
+        {title: "Projects", url: "#Projects"},
+    ]
+</script>
+
+
+<nav>
+    <section class="container_nav" id="/">
+        {#each navItems as {title,url}}
+            <a href={url} class="nav__item">
+                {title}
+            </a>
+        {/each}
+    </section>
+
+</nav>
+
+<style>
+    .container_nav {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+    }
+    .nav__item {
+        color: white;
+        font-weight: 600;
+        font-size: 2rem;
+        margin: 0 2rem;
+        transition: 400ms all;
+    }
+
+    .nav__item:hover {
+        color: rgb(0, 153, 255);
+    }
+
+
+</style>
