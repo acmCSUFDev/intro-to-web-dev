f9bd9f7ea4caabf29d22dd2b15a6e451ee66f417
anhduy1202 <aduy1122@gmail.com>
2022-10-14 16:11:13 -0700

    update semantic tag

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,41 +1,45 @@
 <script>
-    const navItems = [
-        {title: "About", url: "#About"},
-        {title: "Experiences", url: "#Experiences"},
-        {title: "Projects", url: "#Projects"},
-    ]
+  const navItems = [
+    { title: "About", url: "#About" },
+    { title: "Experiences", url: "#Experiences" },
+    { title: "Projects", url: "#Projects" },
+  ];
 </script>
 
-
 <nav>
-    <section class="container_nav" id="/">
-        {#each navItems as {title,url}}
-            <a href={url} class="nav__item">
-                {title}
-            </a>
-        {/each}
-    </section>
-
+  <ol class="container_nav" id="/">
+    {#each navItems as { title, url }}
+      <li>
+        <a href={url} class="nav__item">
+          {title}
+        </a>
+      </li>
+    {/each}
+  </ol>
 </nav>
 
 <style>
-    .container_nav {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: row;
-    }
-    .nav__item {
-        color: white;
-        font-weight: 600;
-        font-size: 2rem;
-        margin: 0 2rem;
-        transition: 400ms all;
-    }
+  ol {
+    list-style: none;
+  }
+  li {
+    padding: 0;
+  }
+  .container_nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+  .nav__item {
+    color: white;
+    font-weight: 600;
+    font-size: 2rem;
+    margin: 0 2rem;
+    transition: 400ms all;
+  }
 
-    .nav__item:hover {
-        color: rgb(0, 153, 255);
-    }
-
-
+  .nav__item:hover {
+    color: rgb(0, 153, 255);
+  }
 </style>
